e2399bbc8bddc0f0ec9be3b605c5842642146c2f
David Malinge <david.malinge@gmail.com>
2021-12-02 08:18:21 +0100

    add lib/utils

---
+++ src/utils.rs
@@ -0,0 +1,14 @@
+#[allow(unused_macros)]
+#[cfg(debug_assertions)]
+macro_rules! debug {
+    ($x:expr) => {
+        dbg!($x)
+    };
+}
+
+#[cfg(not(debug_assertions))]
+macro_rules! debug {
+    ($x:expr) => {
+        std::convert::identity($x)
+    };
+}
